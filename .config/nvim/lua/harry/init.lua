30854054ff7a9df2d1407cd9c56b5a43f0ca3ffd
harry <dev@harrybird.nz>
2025-06-24 11:21:51 +1200

    added files

---
+++ .config/nvim/lua/harry/init.lua
@@ -0,0 +1,7 @@
+require("harry.set")
+require("harry.remap")
+require("harry.lazy_init")
+
+vim.g.netrw_browse_split = 0
+vim.g.netrw_banner = 0
+vim.g.netrw_winsize = 25
---
+++ .config/nvim/lua/harry/remap.lua
@@ -0,0 +1,5 @@
+vim.g.mapleader = " "
+vim.keymap.set("n", "<leader>fv", vim.cmd.Ex)
+vim.keymap.set("n", "<leader><CR>", "<cmd>so ~/.config/nvim.init.lua<CR>") 
+vim.keymap.set("n", "<leader>a", "<cmd>e #<CR>") 
+
